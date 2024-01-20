d21b71e511b246bcfff19ec076a3d2ae9ce51340
femailman <93400575+femailman@users.noreply.github.com>
2024-01-20 04:40:37 -0500

    Set up router for signing up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
 // import LoginPage from './pages/LoginPage';
-import WelcomeInput from './pages/WelcomeInput';
+import VerificationPage from './pages/VerificationPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
+import NoPage from './pages/NoPage';
+import SignupPage from './pages/SignupPage';
+import PasswordPage from './pages/PasswordPage';
 
 function App() {
-  return <WelcomeInput />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<WelcomePage />} />
+        <Route path='signup'>
+          <Route index element={<SignupPage />} />
+          <Route path='verification' element={<VerificationPage />} />
+          <Route path='password' element={<PasswordPage />} />
+        </Route>
+        <Route path='*' element={<NoPage />} />{' '}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
