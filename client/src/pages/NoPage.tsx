ec61ce84399ea0b91f3eb27839dc25ba0fdba529
snavemail <98936310+snavemail@users.noreply.github.com>
2024-01-24 20:58:02 -0500

    Pretty up no page

--- client/src/pages/NoPage.tsx
+++ client/src/pages/NoPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Container, MainHeader, MainWrapper } from '../styles/LoginPageStyles';
+import { useTheme } from './Login/Signup/DarkThemeContex';
 
 export default function NoPage() {
+  const { darkMode } = useTheme();
   return (
-    <Container>
-      <MainWrapper>
-        <MainHeader>Sorry, the page you are looking for does not exist.</MainHeader>
+    <Container darkMode={darkMode}>
+      <MainWrapper darkMode={darkMode}>
+        <MainHeader darkMode={darkMode}>
+          Error 404, Sorry, the page you are looking for does not exist.
+        </MainHeader>
       </MainWrapper>
     </Container>
   );
