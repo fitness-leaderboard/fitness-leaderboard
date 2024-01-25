15d372aa555512fb2894f7c1080a382d79f3c5c2
snavemail <98936310+snavemail@users.noreply.github.com>
2024-01-24 20:57:11 -0500

    Add login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignupPage from './pages/Login/Signup/SignupPage';
-import WelcomePage from './pages/Login/Signup/WelcomePage';
+import LoginPage from './pages/Login/Signup/LoginPage';
 import VerificationPage from './pages/Login/Signup/VerificationPage';
 import PasswordPage from './pages/Login/Signup/PasswordPage';
 import NoPage from './pages/NoPage';
+import HomePage from './pages/HomePage';
 // import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<WelcomePage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path='signup'>
           <Route index element={<SignupPage />} />
           <Route path='verification' element={<VerificationPage />} />
