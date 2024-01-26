81bcd68b2e152721caf8d96961e265bc1f42752b
Kenny Lin <lin.kenn@northeastern.edu>
2024-01-26 18:35:39 -0500

    heroku root route error

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import emailRoutes from './src/routes/email.router'
+import userRoutes from './src/routes/user.router'
+import rootRoutes from './src/routes/util.router'
+
 import cors from 'cors'
 
 const app = express()
@@ -10,6 +13,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 
 app.use(emailRoutes)
+app.use(userRoutes)
+app.use(rootRoutes)
 
 app.listen(
   PORT,
