52488d7302028b4b7c0a0e14c123da1a81fba367
Kenny Lin <lin.kenn@northeastern.edu>
2024-01-26 18:09:20 -0500

    server port heroku

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import emailRoutes from './src/routes/email.router'
 import cors from 'cors'
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
