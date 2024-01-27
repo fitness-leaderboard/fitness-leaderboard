1ce7eac675aecf62526e0be40cafbaaea89f7499
Kenny Lin <lin.kenn@northeastern.edu>
2024-01-26 19:06:11 -0500

    rmv email routes

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import emailRoutes from './src/routes/email.router'
+//import emailRoutes from './src/routes/email.router'
 import userRoutes from './src/routes/user.router'
 import rootRoutes from './src/routes/util.router'
 
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 
-app.use(emailRoutes)
+//app.use(emailRoutes)
 app.use(userRoutes)
 app.use(rootRoutes)
 
