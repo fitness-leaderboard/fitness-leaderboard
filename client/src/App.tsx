9eec87c7325989146cd8589a2b5d714c3082af9c
snavemail <98936310+snavemail@users.noreply.github.com>
2024-01-28 21:34:12 -0500

    set / to login page for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<LoginPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='signup'>
           <Route index element={<SignupPage />} />
