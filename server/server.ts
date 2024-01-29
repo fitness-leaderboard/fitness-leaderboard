d94aaaef1e243dbce93e834bdfa0e317bc099c1c
snavemail <98936310+snavemail@users.noreply.github.com>
2024-01-29 01:45:52 -0500

    styling

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,18 @@
-import express from 'express'
-import emailRoutes from './src/routes/email.router'
-import cors from 'cors'
+import express from 'express';
+import emailRoutes from './src/routes/email.router';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use(emailRoutes)
+app.use('/', emailRoutes);
 
-app.listen(
-  PORT,
-  () => { console.log(`Listening on http://localhost:${PORT}`) }
-)
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
 
-export default app
+export default app;
