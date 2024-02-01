4c301849a2e28e26fef7a137946fcb930fd9be86
snavemail <liam.jk.evans@gmail.com>
2024-02-01 06:24:58 -0500

    Add authroutes and import cors

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import emailRoutes from './src/routes/email.router'
+import emailRoutes from './src/routes/email.router';
 import stravaRoutes from './src/routes/strava.router';
+import authRoutes from './src/routes/auth.router';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
@@ -9,8 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use(emailRoutes)
+app.use(emailRoutes);
 app.use(stravaRoutes);
+app.use('/', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
