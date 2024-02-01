3529241482add03c247a9ead6764717f4d98cae8
snavemail <liam.jk.evans@gmail.com>
2024-02-01 05:34:02 -0500

    Add in login and registration validators to replace emailValidation

--- server/src/api/auth.api.ts
+++ server/src/api/auth.api.ts
@@ -7,6 +7,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 import * as bcrypt from 'bcrypt';
 import jwtGenerator from '../utils/jwtGenerator';
+import { loginValidator, registrationValidator } from '../utils/validators';
 
 export const register = async (
   req: Request,
@@ -14,65 +15,67 @@ export const register = async (
   //next: NextFunction
 ) => {
   const { email, password } = req.body;
+  const { errors, valid } = registrationValidator(email, password);
 
-  try {
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (existingUser) {
-      return res.status(400).json({ message: 'Email already exists' });
-    }
-
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-
-    const bcryptPassword = await bcrypt.hash(password, salt);
-
-    const user = await prisma.user.create({
-      data: {
-        email: email,
-        password: bcryptPassword,
-      },
-    });
-
-    const jwtToken = jwtGenerator(user);
-
-    return res.status(201).json({
-      id: user.id,
-      username: user.email,
-      jwtToken,
-    });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
+  if (!valid) {
+    return res.status(400).send({ message: Object.values(errors)[0] });
   }
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (existingUser) {
+    return res.status(400).json({ message: 'Email already exists' });
+  }
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  const bcryptPassword = await bcrypt.hash(password, salt);
+
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: bcryptPassword,
+    },
+  });
+
+  const jwtToken = jwtGenerator(user);
+
+  return res.status(201).json({
+    id: user.id,
+    username: user.email,
+    jwtToken,
+  });
 };
 
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
+  const { errors, valid } = loginValidator(email, password);
 
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
-    });
-
-    if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    const jwtToken = jwtGenerator(user);
-    return res.status(200).json(jwtToken);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
+  if (!valid) {
+    return res.status(400).send({ message: Object.values(errors)[0] });
   }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  const jwtToken = jwtGenerator(user);
+  return res.status(200).json(jwtToken);
 };
