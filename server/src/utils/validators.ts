51683bcf6e85b8a9efa969db051748b9ccaeedc1
snavemail <liam.jk.evans@gmail.com>
2024-02-01 06:25:37 -0500

    Check that email has "@"

--- server/src/utils/validators.ts
+++ server/src/utils/validators.ts
@@ -18,12 +18,14 @@ export const registrationValidator = (email: string, password: string) => {
     errors.email = 'Invalid email format';
   }
 
-  // Extract domain from the email
-  const [, domain] = email.split('@');
+  if (email.includes('@')) {
+    // Extract domain from the email
+    const [, domain] = email.split('@');
 
-  // Check if the domain is 'northeastern.edu'
-  if (domain.toLowerCase() !== 'northeastern.edu') {
-    errors.email = 'Please use your @northeastern.edu email address';
+    // Check if the domain is 'northeastern.edu'
+    if (domain.toLowerCase() !== 'northeastern.edu') {
+      errors.email = 'Please use your @northeastern.edu email address';
+    }
   }
 
   if (!password || password.trim() === '' || password.length < 6) {
