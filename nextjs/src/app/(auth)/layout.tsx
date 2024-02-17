4196f71789fb353858fb21cf64eade6c8ae271c8
snavemail <liam.jk.evans@gmail.com>
2024-02-17 05:11:40 -0500

    Protect Auth routes from authorized users

--- nextjs/src/app/(auth)/layout.tsx
+++ nextjs/src/app/(auth)/layout.tsx
@@ -1,6 +1,29 @@
+'use client';
+import { getUser } from '@/lib/getUser';
 import '@css/auth.css';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const validateUser = async () => {
+      const { user } = await getUser();
+      if (user) {
+        console.error(user);
+        router.push('/profile');
+        return;
+      }
+      setIsSuccess(true);
+    };
+    validateUser();
+  }, [router]);
+
+  if (!isSuccess) {
+    return <div>Loading...</div>;
+  }
   return (
     <div
       style={{
--- nextjs/src/app/(root)/layout.tsx
+++ nextjs/src/app/(root)/layout.tsx
@@ -1,31 +1,25 @@
 'use client';
 import NavBar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+import { getUser } from '@/lib/getUser';
 import '@css/sidebar.css';
-import axios, { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-
-interface UserResponse {
-  user: string | null;
-  error: AxiosError | null;
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const router = useRouter();
 
   useEffect(() => {
-    const func = async () => {
-      const { user, error } = await getUser();
+    const validateUser = async () => {
+      const { error } = await getUser();
       if (error) {
-        console.error(error);
         router.push('/login');
         return;
       }
       setIsSuccess(true);
     };
-    func();
+    validateUser();
   }, [router]);
 
   if (!isSuccess) {
@@ -43,20 +37,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </div>
     </main>
   );
-}
-
-async function getUser(): Promise<UserResponse> {
-  try {
-    const { data } = await axios.get('/api/me');
-    return {
-      user: data,
-      error: null,
-    };
-  } catch (err) {
-    const error = err as AxiosError;
-    return {
-      user: null,
-      error,
-    };
-  }
 }
