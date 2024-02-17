c35907bedff7010d9536fd651135f878809ee6ab
snavemail <liam.jk.evans@gmail.com>
2024-02-17 05:45:41 -0500

    User can log out

--- nextjs/src/components/Navbar.tsx
+++ nextjs/src/components/Navbar.tsx
@@ -5,10 +5,12 @@ import { CgProfile } from 'react-icons/cg';
 import { IoMdSync } from 'react-icons/io';
 import { CgLogOut } from 'react-icons/cg';
 import Link from 'next/link';
+import axios from 'axios';
 
 export default function NavBar() {
-  const logOut = () => {
-    console.log('Logged out');
+  const logOut = async () => {
+    await axios.post('/api/logout');
+    window.location.reload();
   };
 
   return (
@@ -16,17 +18,44 @@ export default function NavBar() {
       <div>Close</div>
       <div className='navbar-header-nav'>
         <div className='navbar-header-nav-item'>
-          <button className='navbar-header-nav-link' onClick={logOut}>
+          <button
+            className='navbar-header-nav-link'
+            style={{
+              backgroundColor: 'transparent',
+              color: 'black',
+              outline: 'none',
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            onClick={logOut}>
             <CgLogOut />
           </button>
         </div>
         <div className='navbar-header-nav-item'>
-          <Link className='navbar-header-nav-link' href='#'>
+          <Link
+            className='navbar-header-nav-link'
+            href='#'
+            style={{
+              backgroundColor: 'transparent',
+              color: 'black',
+              outline: 'none',
+              border: 'none',
+              cursor: 'pointer',
+            }}>
             <IoMdSync />
           </Link>
         </div>
         <div className='navbar-header-nav-item'>
-          <Link className='navbar-header-nav-link' href='/profile'>
+          <Link
+            className='navbar-header-nav-link'
+            href='/profile'
+            style={{
+              backgroundColor: 'transparent',
+              color: 'black',
+              outline: 'none',
+              border: 'none',
+              cursor: 'pointer',
+            }}>
             <CgProfile />
           </Link>
         </div>
