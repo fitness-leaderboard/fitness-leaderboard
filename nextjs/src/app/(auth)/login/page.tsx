05c287b1ddce38fa8376492fa2eca4e959ef59a5
snavemail <liam.jk.evans@gmail.com>
2024-02-17 03:06:18 -0500

    Clean up signin form

--- nextjs/src/app/(auth)/login/page.tsx
+++ nextjs/src/app/(auth)/login/page.tsx
@@ -1,12 +1,10 @@
-'use client';
-
-import SignupForm from './signup-form';
+import SigninForm from './signin-form';
 
 export default function Home() {
   return (
     <div style={{ width: '100%', padding: 30, justifyContent: 'center', alignContent: 'center' }}>
       <h1>Signup</h1>
-      <SignupForm />
+      <SigninForm />
     </div>
   );
 }
--- nextjs/src/app/api/(auth)/login/route.ts
+++ nextjs/src/app/api/(auth)/login/route.ts
@@ -3,11 +3,13 @@ const prisma = new PrismaClient();
 import * as bcrypt from 'bcrypt';
 import { loginValidator } from '@/lib/validators';
 import { signJwtAccessToken } from '@lib/jwt';
-import { NextRequest } from 'next/server';
-import { NextApiRequest } from 'next/types';
+import { NextRequest, NextResponse } from 'next/server';
+import { serialize } from 'cookie';
 
 export async function POST(request: NextRequest) {
-  const { email, password } = await request.json();
+  const body = await request.json();
+
+  const { email, password } = body;
 
   console.log('email:', email);
   console.log('password:', password);
@@ -15,7 +17,7 @@ export async function POST(request: NextRequest) {
   const { errors, valid } = loginValidator(email, password);
 
   if (!valid) {
-    return new Response(JSON.stringify(errors), {
+    return new NextResponse(JSON.stringify(errors), {
       status: 400,
       headers: { 'Content-Type': 'application/json' },
     });
@@ -29,18 +31,35 @@ export async function POST(request: NextRequest) {
 
   if (user && (await bcrypt.compare(password, user.password))) {
     const { password, createdAt, updatedAt, ...rest } = user;
-    const jwtToken = signJwtAccessToken(rest);
+
+    const MAX_AGE = 60 * 60 * 24 * 7;
+
+    const expires = {
+      expiresIn: 60 * 60 * 24 * 7,
+    };
+
+    const jwtToken = signJwtAccessToken(rest, expires);
+
+    const serializedToken = serialize('OutSiteJWT', jwtToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: MAX_AGE,
+      path: '/',
+    });
 
     const result = {
+      message: 'Login successful',
+      // These two will not be needed, cookie has jwtToken and ...rest is unneeded cuz of token
       ...rest,
       jwtToken,
     };
     return new Response(JSON.stringify(result), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Set-Cookie': serializedToken, 'Content-Type': 'application/json' },
     });
   } else {
-    return new Response(JSON.stringify({ error: 'Invalid credentials' }), {
+    return new NextResponse(JSON.stringify({ error: 'Invalid credentials' }), {
       status: 401,
       headers: { 'Content-Type': 'application/json' },
     });
