119aa1d863ace394120d71d15363efee9ee178fd
Kenny Lin <lin.kenn@northeastern.edu>
2024-02-23 00:17:35 -0500

    challenge page basic design

--- nextjs/src/app/(root)/challenges/page.tsx
+++ nextjs/src/app/(root)/challenges/page.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Grid, Paper, Typography, Box } from '@mui/material';
+import { ChallengeList } from '@/components/Challenges/ChallengeList';
+import { BadgeList } from '@/components/Badges/BadgeList';
+import '@css/list.css';
 
 export default function Challenges() {
-  return <div>Challenges</div>;
+
+  return (
+      <Grid container padding={10} spacing={10}>
+        <Grid item xs={12}>
+          <Typography variant='subtitle1' fontWeight='light'>Challenges</Typography>  
+          <Typography variant='h4'>Join a Challenge</Typography>  
+        </Grid>
+        <Grid item xs={12}>  
+          <Paper elevation={4} sx={{padding: 1, backgroundColor: '#FAFAFA'}}>
+            <ChallengeList />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='h4'>Your Badges</Typography>
+          <BadgeList/>
+        </Grid>
+
+      </Grid>
+  )
 }
