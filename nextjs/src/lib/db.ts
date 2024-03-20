deeb43bb97bdaffcf4dfd56c758af4237b0504ae
Kenny Lin <lin.kenn@northeastern.edu>
2024-03-19 21:21:56 -0400

    singleton prisma client

--- nextjs/src/lib/db.ts
+++ nextjs/src/lib/db.ts
@@ -1,4 +1,9 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
 
-export default prisma;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
